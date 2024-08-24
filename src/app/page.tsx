b65df16481920c4bc93e75b90619840f3cd97629
Okunola Olubanjo <okunolaolubanjo@gmail.com>
2024-08-24 15:43:14 +0100

    Worked on patient page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,7 @@
 
-import Nav from "./Nav/Nav";
-import Sidebar from "@/components/sidebar/sidebar";
-import Avatar from "../assets/img/profile.jpg"
-
-
 export default function Home() {
   return (
     <main className="pt-4">
-      <div className="flex flex-row flex-wrap">
-      <Sidebar 
-      avatar={Avatar}
-      heading="Richard Ronalds"
-      address="Preston Inglewood Maine 98380"
-      />
-      </div>
      
     </main>
   );
