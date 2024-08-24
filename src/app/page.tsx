aea9c035777f7226e47c161a91abec8e094f61dd
kingjohneycodey <120632471+Kingjohneycodey@users.noreply.github.com>
2024-08-24 16:06:08 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
 
 export default function Home() {
   return (
-    <main className="pt-4">
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1>Health App</h1>
      
     </main>
   );
